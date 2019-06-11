171ab7b60f88915e70348b20445ebfee338c721e
edsandorf <esandorf@gmail.com>
2019-06-11 16:07:42 +0100

    Added namespace import runif

---
+++ R/obfuscatoR.R
@@ -0,0 +1,14 @@
+#' obfuscatoR: Designs and analysis of the obfuscation game
+#' 
+#' This package creates designs for the obfuscation game. It has a simple 
+#' interface where you specify the number of strategies and outcomes. Given a  
+#' set of constraints, feasible designs are provided that can be readily  
+#' implemented to replicate results or extend the game to test new types of  
+#' behavior.
+#'  
+#' @importFrom stats runif
+#'  
+#' @docType package
+#' @name obfuscatoR
+#'  
+NULL
