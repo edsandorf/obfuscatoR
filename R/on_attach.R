f10decb884f9fcc7f3fa418dd53324ef64f80c8a
edsandorf <esandorf@gmail.com>
2019-06-11 16:08:03 +0100

    Implemented a trycatch procedure

--- R/on_attach.R
+++ R/on_attach.R
@@ -10,9 +10,19 @@
 .onAttach <- function(libname, pkgname){
     installed_version <- utils::packageDescription("obfuscatoR", fields = "Version")
     
-    # description <- readLines("https://raw.githubusercontent.com/edsandorf/obfuscatoR/master/DESCRIPTION")
-    # remote_version <- gsub("Version:\\s*", "", description[grep('Version:', description)])
-    remote_version <- "NA"
+    description <- tryCatch({
+        readLines("https://raw.githubusercontent.com/edsandorf/obfuscatoR/master/DESCRIPTION")
+    }, warning = function(w) {
+        return("NA")
+    }, error = function(e) {
+        return("NA")
+    })
+    
+    if (description == "NA") {
+        remote_version <- description
+    } else {
+        remote_version <- gsub("Version:\\s*", "", description[grep('Version:', description)])
+    }
     
     packageStartupMessage("Welcome to obfuscatoR! \n\n",
                           "You are currently using version: ",
