21e25beeb587b3c059b04a27b52b93f1ee23fbbe
edsandorf <esandorf@gmail.com>
2019-06-19 16:41:54 +0100

    Outlined and moved print methods

--- R/print_methods.R
+++ R/print_methods.R
@@ -2,9 +2,9 @@
 #' 
 #' Prints the ra_matrix with the considered rule highlighted
 #' 
-#' @ra_mat A matrix with rows equal to the number of rules and columns
+#' @param ra_mat A matrix with rows equal to the number of rules and columns
 #' equal to the number of actions
-#' @c_rule Defaults to the rule specified in design_opt. NOTE: If you are only 
+#' @param c_rule Defaults to the rule specified in design_opt. NOTE: If you are only 
 #' evaluating a rules and action matrix, you must remember to set the c_rule or
 #' it will automatically default to 5. 
 #' 
@@ -12,7 +12,22 @@
 #' 
 #' @export
 
-print_ra_mat <- function(ra_mat, c_rule = design_opt$considered_rule) {
+print_ra_mat <- function(ra_mat, c_rule = NULL) {
   
   
 }
+
+#' Function for printing vectors and matrices 
+#' 
+#' @param X Input matrix
+#' @param row_wise If TRUE populate something row_wise. Default FALSE
+#' @param digits Number of digits for rounding
+#' 
+#' @export
+
+print_object <- function(X, row_wise = FALSE, digits = 2){
+  if(is.matrix(X)){
+    
+  }
+}
+
