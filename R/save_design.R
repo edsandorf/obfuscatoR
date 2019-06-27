585722c5d2a4aacf0c61acc3eee7c2553ea36924
edsandorf <esandorf@gmail.com>
2019-06-27 16:25:47 +0100

    Updated save_design() - needs testing

--- R/save_design.R
+++ R/save_design.R
@@ -1,10 +1,9 @@
 #' Save obfuscation designs
 #'
-#' Function for storing designs. 
+#' Writes the designs to separate .csv files. 
 #'
 #' @param x A design or list of designs
-#' @param x_name A character string with the name and extension of the file to 
-#' to be stored. If file extension is not provided the default is .csv
+#' @param x_name A character string with the name of the file
 #' @param path A string giving the path to where the designs are stored. The 
 #' default is the current working directory
 #'
@@ -24,7 +23,20 @@ save_design <- function(x, x_name,  path = getwd()) {
   }
   
   # Check if extension is part of x_name
+  file_path <- unlist(stringr::str_split(x, "\\."))
+  file_extension <- last(file_path)
+  if (file_extension == "csv") {
+    file_path <- file_path[-c(length(file_path))]
+  }
   
-  # Get the file extension to call the correct function
-  
+  # Check for list of designs
+  if (is.list(x)) {
+    lapply(seq_along(x), function (i) {
+      path <- stringr::str_c(file_path, "-design-", i, ".csv")
+      readr::write_csv(x[[i]], path = path)
+    })
+  } else {
+    path <- stringr::str_c(file_path, ".csv")
+    readr::write_csv(x, path = path)
+  }
 }
